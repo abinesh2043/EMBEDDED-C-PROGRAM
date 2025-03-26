e0884e3c4bb99737baf992a84ce0981aeed2bbde
abinesh2043 <132431568+abinesh2043@users.noreply.github.com>
2025-03-26 10:29:24 +0530

    Add files via upload

---
+++ 16multi.c
@@ -0,0 +1,14 @@
+#include <reg51.h>
+
+void main()
+{
+	while(1)
+	{
+		unsigned int num1, num2;
+		unsigned long int product;
+		num1 = 6;
+		num2 = 8;
+		product = (unsigned long int)num1 * num2;
+		
+} 
+	}
